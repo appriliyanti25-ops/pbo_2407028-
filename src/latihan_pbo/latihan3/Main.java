d5ff8c055dc30fa84074162d0d290d0df16e71a7
Aprilliyanti <appriliyanti25@gmail.com>
2026-04-07 23:04:43 +0700

    Latihan 5: BangunDatar & BangunRuang

--- src/latihan_pbo/latihan3/Main.java
+++ src/latihan_pbo/latihan3/Main.java
@@ -8,8 +8,6 @@ class KamarHotel {
     private String fasilitas1;
     private String fasilitas2;
     private String fasilitas3;
-    private String fasilitas4;
-    private String fasilitas5;
 
     public void setTipeKamar(String tipeKamar) {
         this.tipeKamar = tipeKamar;
@@ -27,21 +25,11 @@ class KamarHotel {
         this.fasilitas3 = fasilitas3;
     }
 
-    public void setFasilitas4(String fasilitas4) {
-        this.fasilitas4 = fasilitas4;
-    }
-
-    public void setFasilitas5(String fasilitas5) {
-        this.fasilitas5 = fasilitas5;
-    }
-
     public void tampil() {
         System.out.println("Tipe Kamar : " + tipeKamar);
         System.out.println("Fasilitas 1: " + fasilitas1);
         System.out.println("Fasilitas 2: " + fasilitas2);
         System.out.println("Fasilitas 3: " + fasilitas3);
-        System.out.println("Fasilitas 4: " + fasilitas4);
-        System.out.println("Fasilitas 5: " + fasilitas5);
     }
 }
 
@@ -51,43 +39,26 @@ public class Main {
         Scanner input = new Scanner(System.in);
         KamarHotel k = new KamarHotel();
 
-        System.out.print("Masukkan kode kamar (1/2/3/4/5): ");
+        System.out.print("Masukkan kode kamar (2/3/4): ");
         int kode = input.nextInt();
 
-        if (kode == 1) {
+        if (kode == 2) {
             k.setTipeKamar("Standard");
             k.setFasilitas1("Bed Single");
             k.setFasilitas2("AC");
-            k.setFasilitas3("Kamar Mandi private");
+            k.setFasilitas3("Kamar Mandi");
 
-        } else if (kode == 2) {
+        } else if (kode == 3) {
             k.setTipeKamar("Deluxe");
             k.setFasilitas1("Bed Queen");
             k.setFasilitas2("AC");
             k.setFasilitas3("TV");
-            k.setFasilitas4("Kamar Mandi private");
 
-        } else if (kode == 3) {
+        } else if (kode == 4) {
             k.setTipeKamar("Suite");
             k.setFasilitas1("Bed King");
             k.setFasilitas2("AC");
             k.setFasilitas3("TV + Bathtub");
-
-        } else if (kode == 4) {
-            k.setTipeKamar("Primier");
-            k.setFasilitas1("Bed King");
-            k.setFasilitas2("AC");
-            k.setFasilitas3("TV + Bathtub");
-            k.setFasilitas4("Balkon");
-
-         } else if (kode == 5) {
-            k.setTipeKamar("Premium");
-            k.setFasilitas1("Bed King");
-            k.setFasilitas2("AC");
-            k.setFasilitas3("TV + Bathtub");
-            k.setFasilitas4("Balkon");
-            k.setFasilitas5("Kolam Renang Private");
-
 
         } else {
             System.out.println("Kode kamar tidak tersedia");
