9c2718bb832e4ed13dcf2ddc5d52bc7c6eebfb2d
Aprilliyanti <appriliyanti25@gmail.com>
2026-04-07 23:57:13 +0700

    Latihan 5

---
+++ latihan_2407028/src/latihan_pbo/latihan5/BangunRuang.java
@@ -0,0 +1,14 @@
+package latihan_pbo.latihan5;
+
+public class BangunRuang extends BangunDatar {
+    private double tinggi;
+
+    public BangunRuang(double panjang, double lebar, double tinggi) {
+        super(panjang, lebar); 
+        this.tinggi = tinggi;
+    }
+
+    public double hitungVolume() {
+        return hitungLuas() * tinggi; 
+    }
+}
